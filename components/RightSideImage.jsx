9f3b10652709e3b7295d3972c85fcc5c647236aa
Fernando Torres <ferwebz@gmail.com>
2022-03-08 12:54:11 -0600

    cambios de estructura en pantalla anfitrion paso 1

--- components/RightSideImage.jsx
+++ components/RightSideImage.jsx
@@ -5,7 +5,8 @@ const RightSideImage = (props) => {
   const { image, title, subtitle, alt } = props;
   return (
     <section className={RightSideImageStyles.right_side_image}>
-      <Image src={ image } alt={ alt } layout="fill" />
+      <div className={RightSideImageStyles.coverImage}></div>
+      {/*<Image src={ image } alt={ alt } layout="fill" />*/}
       <h2 className={RightSideImageStyles.carlos}> { title } </h2>
       <p className={RightSideImageStyles.subtitle}> { subtitle } </p>
     </section>
