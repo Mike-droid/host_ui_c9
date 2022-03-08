778135c014240d7ded5249a8cf99944335846d82
Fernando Torres <ferwebz@gmail.com>
2022-03-08 12:30:55 -0600

    Cambios de estilos para responsive en pantallas host

--- components/LeftSideWithButton.jsx
+++ components/LeftSideWithButton.jsx
@@ -6,11 +6,16 @@ const LeftSideWithButton = (props) => {
   const { description } = props;
   return (
     <section className={LeftSideWithButtonStyles.leftSideWithButton}>
+
       <BSbutton />
-      <p className={LeftSideWithButtonStyles.description}>
-        { description }
-      </p>
-      <ActionButton textButton="Anímate a ser anfitrión" paso={1} />
+
+      <div className={LeftSideWithButtonStyles.innerContent}>
+        <p className={LeftSideWithButtonStyles.description}>
+          { description }
+        </p>
+        <ActionButton textButton="Anímate a ser anfitrión" paso={1} />
+      </div>
+
     </section>
   )
 }
